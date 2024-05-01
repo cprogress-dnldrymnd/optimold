fd65c02188e111ddab88a6ebf87b2b7f61cd06de
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-01 18:09:43 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,9 @@
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
+   wp_enqueue_style( 'swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css' );
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+   wp_enqueue_script( 'swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js' );
 }
 
 require_once('includes/wp-bakery.php');
